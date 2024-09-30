e6a0a1708e007c204032e3b9f19dbfce9377d4e7
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2024-09-29 22:40:02 -0300

    Atividade terminada

--- bd3-atv2-alex/find.mongodb.js
+++ bd3-atv2-alex/find.mongodb.js
@@ -1,8 +1,8 @@
-/*NOME DO BANCO DE DADOS*/
-const database = "bd3_atv2"
+/* NOME DO BANCO DE DADOS */
+const database = "bd3_atv2";
 
 /* NOME DO BANCO DA COLEÇÃO DE DADOS: */
-const collection = "bd3_atv2_produtos"
+const collection = "bd3_atv2_produtos";
 
 /* CRIAR OU ACESSAR O BANCO DE DADOS: */
 use(database);
